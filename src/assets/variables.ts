e11f752913951c531f96549bb0d26d36ead85fce
dmitry <joydnby@gmail.com>
2020-10-27 12:21:56 +0300

    refactor: create variables.ts, move colors to styles.css

---
+++ src/assets/variables.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-inferrable-types */
+export enum Colors {
+  green = '#228B22',
+  blue = 	'#0000FF',
+}
+
+export const dayInMs: number = 86400000;
+export const requiredDays: number = 14;
