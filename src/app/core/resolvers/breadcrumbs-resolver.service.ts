185de5927b9976d7aaeddd52df59ab4f69c290f8
dmitry <joydnby@gmail.com>
2020-11-29 01:19:00 +0300

    refactor: breadcrumbs resolver

--- src/app/core/resolvers/breadcrumbs-resolver.service.ts
+++ src/app/core/resolvers/breadcrumbs-resolver.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
 import { Course } from 'src/app/shared/models/course';
 import { BreadcrumbsResolverData } from 'src/app/shared/models/breadcrumbs';
+import { switchMap, take } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,17 +13,26 @@ export class BreadcrumbsResolverService implements Resolve<BreadcrumbsResolverDa
 
   constructor(private coursesService: CoursesService) { }
 
-  public resolve(route: ActivatedRouteSnapshot): BreadcrumbsResolverData {
+  public resolve(route: ActivatedRouteSnapshot): Observable<BreadcrumbsResolverData> {
     let breadcrumbs: string;
+
     const {id} = route.params;
     const {page} = route.data;
-    const course: Course = this.coursesService.getItemById(id);
-    if (page === 'New course') {
-      breadcrumbs = `/ ${page as string}`;
-    }
-    if (course) {
-      breadcrumbs = `/ ${course.title}`;
-    }
-    return {breadcrumbs, course};
+    return this.coursesService.getItemById(id).pipe(
+      take(1),
+      switchMap((course: Course) => {
+        console.log('🚀 ~  switchMap ~ course', course);
+
+        if (page === 'New course') {
+          breadcrumbs = `/ ${page as string}`;
+        }
+        if (course) {
+          breadcrumbs = `/ ${course.name}`;
+        }
+        console.log({breadcrumbs, course});
+        return of({breadcrumbs, course});
+      })
+    );
+
   }
 }
