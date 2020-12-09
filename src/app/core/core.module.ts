d1e54b799093c85cb438c26b54c2ceb6c0304fbd
dmitry <joydnby@gmail.com>
2020-12-09 23:36:21 +0300

    feat: add loading spinner, service and interceptor

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,13 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ApiCallInterceptor } from './interceptors/api-call.interceptor';
+import { LoaderInterceptor } from './interceptors/loader.interceptor';
 @NgModule({
   imports: [
     HttpClientModule],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: ApiCallInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: ApiCallInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true }
   ]
 })
 export class CoreModule {
